07397d6da8cbe048cc0c09e2ec4b6bc0c9edfbf6
zguo123 <41599103+zguo123@users.noreply.github.com>
2020-05-24 22:04:26 -0400

    changed settings

--- ryuclient/settings.py
+++ ryuclient/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qxw2bu=n2&gm@#ceitk*c+2#hmn00asxi4hti=k=bm8v8!ydsb'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
